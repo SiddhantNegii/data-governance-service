f33270e9235d53c33ba72918853a54e6f7db0c4c
siddhant-tartan <siddhant@tartanhq.com>
2026-03-10 10:24:35 +0530

    enhanced basic functionality

--- backend/app/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class DataRecordCreate(BaseModel):
-    uid: str = Field(..., min_length=1)
-    product_id: str = Field(..., min_length=1)
-    data_retention_days: int = Field(..., gt=0)
